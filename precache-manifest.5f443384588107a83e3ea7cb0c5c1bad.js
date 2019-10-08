32e5bff49dca0a13159b0f5b75f9d994e0e0094e
Ula <ulaa93@gmail.com>
2019-10-08 12:12:43 +0200

    Updates

---
+++ precache-manifest.5f443384588107a83e3ea7cb0c5c1bad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb10101b4a1c0e5bf09fa9ef42642946",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "a34ba6c7b3a813fa21cd",
+    "url": "/movie-app/static/js/2.70245425.chunk.js"
+  },
+  {
+    "revision": "c156606e62a5b4a924c2",
+    "url": "/movie-app/static/js/main.56129868.chunk.js"
+  },
+  {
+    "revision": "c2f1bd05aad80b3a8409",
+    "url": "/movie-app/static/js/runtime-main.76936bd4.js"
+  }
+]);
